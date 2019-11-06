54de00abd77953de86d7d784bb12b937ebc7922d
Göksel Küçükşahin <gokselkucuksahin@gmail.com>
2019-11-07 02:03:59 +0300

    Add files via upload

--- JCode/src/Main.java
+++ JCode/src/Main.java
@@ -6,10 +6,10 @@ public class Main
     public static void main(String[] args)
     {
 
-        for (int i = 0; i < 20; i++)
+        /*for (int i = 0; i < 20; i++)
         {
             System.out.println("JCode = '" + jCode(Utils.getRandomInt(100000, 999999)) + "'");
-        }
+        }*/
 
 
         Scanner scanner = new Scanner(System.in);
@@ -27,7 +27,9 @@ public class Main
                 System.out.println("err");
                 val = 0;
             }
-            System.out.println("JCode = '" + jCode(val) + "'");
+            String code = jCode(val);
+            System.out.println("JCode = '" + code + "'");
+            System.out.println(isGenuineJCode(code));
         }
     }
 
@@ -35,14 +37,14 @@ public class Main
     {
         StringBuilder sb = new StringBuilder(9);
         sb.append('J'); // Always starts with J
-        String str = Integer.toString(seed);
+        String str = Integer.toString(seed); // int to string
         int sum = 0;
         if (str.length() >= 6)
         {
             // exact size
             for (int i = 0; i < 6; i++)
             {
-                sb.append(str.charAt(i));
+                sb.append(str.charAt(i)); // append all digits to string builder
                 sum += Integer.parseInt(Character.toString(str.charAt(i)));
             }
         } else
@@ -51,15 +53,40 @@ public class Main
             // fill with 0's
             for (int i = 0; i < 6 - str.length(); i++)
             {
-                sb.append("0");
+                sb.append("0"); // fill spaces with 0's
             }
             for (int i = 0; i < str.length(); i++)
             {
-                sb.append(str.charAt(i));
-                sum += Integer.parseInt(Character.toString(str.charAt(i)));
+                sb.append(str.charAt(i)); // append that character to stringbuilder.
+                sum += Integer.parseInt(Character.toString(str.charAt(i))); // get chatAt i th index and add
             }
         }
-        sb.append(String.format("%02d", sum));
+        sb.append(String.format("%02d", sum)); // 02 = 2 digit and if less than 2 digit fill with 0
         return sb.toString();
+    }
+
+    private static boolean isGenuineJCode(String code)
+    {
+        if (code.length() == 9)
+        {
+            if (code.charAt(0) == 'J')
+            {
+                // if Starts with 'J'
+                int sum = 0;
+                for (int i = 1; i < code.length() - 2; i++)
+                {
+                    sum += Integer.parseInt(Character.toString(code.charAt(i))); // check sum
+                }
+                return sum == Integer.parseInt(code.substring(7, 9)); // last 2 digits is same as check sum
+            } else
+            {
+                // Must be J if isn't it's not genuine
+                return false;
+            }
+        } else
+        {
+            // it must be exactly 9 digits
+            return false;
+        }
     }
 }
