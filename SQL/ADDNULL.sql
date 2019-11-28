c0870160e77946dd4aa93e398ebf855421194837
Göksel Küçükşahin <gokselkucuksahin@gmail.com>
2019-11-29 01:51:13 +0300

    Add files via upload

---
+++ SQL/ADDNULL.sql
@@ -0,0 +1,51 @@
+select * from personnel_table;
+insert into personnel_table values (null,'null','null','null','2000-01-01','1000-01-01');
+/*A*/
+select * from machine_table;
+insert into machine_table values(null, 'null','null',1234);
+/*B*/
+select * from couch_casing_table;
+insert into couch_casing_table values (null,1,1,'1000-01-01');
+/*C*/
+select * from couch_cut_fabric_table;
+insert into couch_cut_fabric_table values (null,1,1,'1000-01-01');
+/*D*/
+select * from couch_cut_harboard_table;
+insert into couch_cut_harboard_table values (null,1,1,'1000-01-01');
+/*E*/
+select * from couch_dressing_fabric_table;
+insert into couch_dressing_fabric_table values (null,1,'1000-01-01');
+/*F*/
+select * from couch_drill_harboard_table;
+insert into couch_drill_harboard_table values (null,1,1,'1000-01-01');
+/*G*/
+select * from couch_frame_cut_table;
+insert into couch_frame_cut_table values (null,1,1,'1000-01-01');
+/*H*/
+select * from couch_furnish_fabric_table;
+insert into couch_furnish_fabric_table values (null,1,1,1,'1000-01-01');
+/*I*/
+select * from couch_l_cut_harboard_table;
+insert into couch_l_cut_harboard_table values (null,1,1,'1000-01-01');
+/*J*/
+select * from couch_package_table;
+insert into couch_package_table values (null,1,'1000-01-01');
+/*K*/
+select * from couch_quilt_fabric_table;
+insert into couch_quilt_fabric_table values (null,1,1,'1000-01-01');
+/*L*/
+select * from couch_rubber_pile_table;
+insert into couch_rubber_pile_table values (null,1,1,1,1,'1000-01-01');
+/*M*/
+select * from couch_sew_fabric_table;
+insert into couch_sew_fabric_table values (null,1,1,'1000-01-01');
+/*N*/
+select * from couch_strip_cut_harboard_table;
+insert into couch_strip_cut_harboard_table values (null,1,1,'1000-01-01');
+/*O*/
+select * from couch_whitening_sponge_table;
+insert into couch_whitening_sponge_table values (null,1,'1000-01-01');
+/*P*/
+select * from couch_wrapping_cardboard_table;
+insert into couch_wrapping_cardboard_table values (null,1,'1000-01-01');
+/*Q*/
---
+++ SQL/PROCESS_CREATE_DB_SQL.sql
@@ -0,0 +1,160 @@
+create schema mercury;
+use mercury;
+
+create table personnel_table(
+	id int primary key not null auto_increment,
+    name varchar(40) not null,
+    surname varchar(40) not null,
+    tc varchar(11) not null,
+	workStartDate date not null,
+    birthDate date not null
+);
+
+create table machine_table(
+	id int primary key not null auto_increment,
+    name varchar(20) null default 'default_machine',
+	brand varchar(20) null default 'null_brand',
+    modelDate int null default 0000
+);
+
+create table couch_cut_fabric_table(
+	id int primary key not null auto_increment,
+    personnel1_id int null default 1,
+    personnel2_id int null default 1,
+    processDate datetime not null,
+    foreign key (personnel1_id) references personnel_table(id),
+    foreign key (personnel2_id) references personnel_table(id)
+);
+
+create table couch_sew_fabric_table(
+	id int primary key not null auto_increment,
+    personnel1_id int null default 1,
+    machine_id int null default 1,
+    processDate datetime not null,
+    foreign key (personnel1_id) references personnel_table(id),
+    foreign key (machine_id) references machine_table(id)
+);
+
+create table couch_quilt_fabric_table(
+	id int primary key not null auto_increment,
+    personnel1_id int null default 1,
+    machine_id int null default 1,
+    processDate datetime not null,
+    foreign key (personnel1_id) references personnel_table(id),
+    foreign key (machine_id) references machine_table(id)
+);
+
+create table couch_cut_harboard_table(
+	id int primary key not null auto_increment,
+    personnel1_id int null default 1,
+    machine_id int null default 1,
+    processDate datetime not null,
+    foreign key (personnel1_id) references personnel_table(id),
+    foreign key (machine_id) references machine_table(id)
+);
+
+create table couch_strip_cut_harboard_table(
+	id int primary key not null auto_increment,
+    personnel1_id int null default 1,
+    machine_id int null default 1,
+    processDate datetime not null,
+    foreign key (personnel1_id) references personnel_table(id),
+    foreign key (machine_id) references machine_table(id)
+);
+
+create table couch_l_cut_harboard_table(
+	id int primary key not null auto_increment,
+    personnel1_id int null default 1,
+    machine_id int null default 1,
+    processDate datetime not null,
+    foreign key (personnel1_id) references personnel_table(id),
+    foreign key (machine_id) references machine_table(id)
+);
+
+create table couch_drill_harboard_table(
+	id int primary key not null auto_increment,
+    personnel1_id int null default 1,
+    machine_id int null default 1,
+    processDate datetime not null,
+    foreign key (personnel1_id) references personnel_table(id),
+    foreign key (machine_id) references machine_table(id)
+);
+
+create table couch_casing_table(
+	id int primary key not null auto_increment,
+    personnel1_id int null default 1,
+    personnel2_id int null default 1,
+    processDate datetime not null,
+    foreign key (personnel1_id) references personnel_table(id),
+    foreign key (personnel2_id) references personnel_table(id)
+);
+
+create table couch_frame_cut_table(
+	id int primary key not null auto_increment,
+    personnel1_id int null default 1,
+    personnel2_id int null default 1,
+    processDate datetime not null,
+    foreign key (personnel1_id) references personnel_table(id),
+    foreign key (personnel2_id) references personnel_table(id)
+);
+
+create table couch_rubber_pile_table(
+	id int primary key not null auto_increment,
+    personnel1_id int null default 1,
+    personnel2_id int null default 1,
+	personnel3_id int null default 1,
+	personnel4_id int null default 1,
+    processDate datetime not null,
+    foreign key (personnel1_id) references personnel_table(id),
+    foreign key (personnel2_id) references personnel_table(id),
+	foreign key (personnel3_id) references personnel_table(id),
+    foreign key (personnel4_id) references personnel_table(id)
+);
+
+create table couch_frame_cut_table(
+	id int primary key not null auto_increment,
+    personnel1_id int null default 1,
+    personnel2_id int null default 1,
+    processDate datetime not null,
+    foreign key (personnel1_id) references personnel_table(id),
+    foreign key (personnel2_id) references personnel_table(id)
+);
+
+create table couch_whitening_sponge_table(
+	id int primary key not null auto_increment,
+    personnel1_id int null default 1,
+    processDate datetime not null,
+    foreign key (personnel1_id) references personnel_table(id)
+);
+
+create table couch_wrapping_cardboard_table(
+	id int primary key not null auto_increment,
+    personnel1_id int null default 1,
+    processDate datetime not null,
+    foreign key (personnel1_id) references personnel_table(id)
+);
+
+create table couch_dressing_fabric_table(
+	id int primary key not null auto_increment,
+    personnel1_id int null default 1,
+    processDate datetime not null,
+    foreign key (personnel1_id) references personnel_table(id)
+);
+
+create table couch_furnish_fabric_table(
+	id int primary key not null auto_increment,
+    personnel1_id int null default 1,
+    personnel2_id int null default 1,
+	personnel3_id int null default 1,
+    processDate datetime not null,
+    foreign key (personnel1_id) references personnel_table(id),
+    foreign key (personnel2_id) references personnel_table(id),
+	foreign key (personnel3_id) references personnel_table(id)
+);
+
+create table couch_package_table(
+	id int primary key not null auto_increment,
+    personnel1_id int null default 1,
+    processDate datetime not null,
+    foreign key (personnel1_id) references personnel_table(id)
+);
