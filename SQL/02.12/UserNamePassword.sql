61f522109558b7739df3cf6181f6b0759fa24c73
Göksel Küçükşahin <gokselkucuksahin@gmail.com>
2019-12-06 02:16:13 +0300

    Delete UserNamePassword.sql

--- SQL/02.12/UserNamePassword.sql
+++
@@ -1,6 +0,0 @@
-create table personnel_password_table(
-id int primary key not null auto_increment,
-username varchar(20) not null,
-password varchar(20) not null,
-foreign key (id) references personnel_table(id)
-);
