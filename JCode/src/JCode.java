3cd7a59d1a66370f426a04e91a091f0feff10f34
Göksel Küçükşahin <gokselkucuksahin@gmail.com>
2019-12-06 10:53:14 +0300

    Update JCode.java

--- JCode/src/JCode.java
+++ JCode/src/JCode.java
@@ -1,12 +1,10 @@
-import java.util.Scanner;
-
 public class JCode
 {
-    private static String GenerateJCode(int seed)
+    private static String jCode(long seed)
     {
         StringBuilder sb = new StringBuilder(9);
         sb.append('J'); // Always starts with J
-        String str = Integer.toString(seed); // int to string
+        String str = Long.toString(seed); // int to string
         int sum = 0;
         if (str.length() >= 6)
         {
@@ -14,7 +12,7 @@ public class JCode
             for (int i = 0; i < 6; i++)
             {
                 sb.append(str.charAt(i)); // append all digits to string builder
-                sum += Integer.parseInt(Character.toString(str.charAt(i)));
+                sum += Character.getNumericValue(str.charAt(i));
             }
         } else
         {
@@ -27,11 +25,12 @@ public class JCode
             for (int i = 0; i < str.length(); i++)
             {
                 sb.append(str.charAt(i)); // append that character to stringbuilder.
-                sum += Integer.parseInt(Character.toString(str.charAt(i))); // get chatAt i th index and add
+                sum += Character.getNumericValue(str.charAt(i)); // get chatAt i th index and add
             }
         }
         sb.append(String.format("%02d", sum)); // 02 = 2 digit and if less than 2 digit fill with 0
         return sb.toString();
+		//Goksel KUCUKSAHIN
     }
 
     private static boolean isGenuineJCode(String code)
@@ -47,7 +46,7 @@ public class JCode
                     // it can be like 'Jellybean' its 9 digit and starting with J but not a JCode;
                     for (int i = 1; i < code.length() - 2; i++)
                     {
-                        sum += Integer.parseInt(Character.toString(code.charAt(i))); // check sum
+                        sum += Character.getNumericValue(code.charAt(i)); // check sum
                     }
                     return sum == Integer.parseInt(code.substring(7, 9)); // last 2 digits is same as check sum
                 }
@@ -66,5 +65,6 @@ public class JCode
             // it must be exactly 9 digits
             return false;
         }
+		//Goksel KUCUKSAHIN
     }
 }
