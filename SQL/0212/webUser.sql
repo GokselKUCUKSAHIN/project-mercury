43a43dd72d59e5a79a49a402ec4db542323b8c31
Göksel Küçükşahin <gokselkucuksahin@gmail.com>
2019-12-07 15:08:42 +0300

    Update and rename SQL/02.12/userNamePassword.sql to SQL/0212/webUser.sql

---
+++ SQL/0212/webUser.sql
@@ -0,0 +1,38 @@
+USE mercury;
+
+create table web_user_table(
+id int primary key not null auto_increment,
+username varchar(20) not null,
+password varchar(20) not null
+);
+
+DELIMITER //
+CREATE PROCEDURE AddNewWebUser(IN userName varchar(20), IN passWord varchar(20))
+BEGIN
+	insert into mercury.web_user_table values (null, userName, passWord);
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE CheckWebUser(IN userName varchar(20))
+BEGIN
+	select (id) from mercury.web_user_table where mercury.web_user_table.username = userName;
+END //
+DELIMITER ;
+
+
+DELIMITER //
+CREATE PROCEDURE DeleteWebUser(IN userName varchar(20))
+BEGIN
+	set @var := (select id from mercury.web_user_table where mercury.web_user_table.username = userName);
+    delete from  mercury.web_user_table where mercury.web_user_table.id = @var;
+END //
+DELIMITER ;
+
+
+DELIMITER // 
+CREATE PROCEDURE CheckWebPassword(IN userName varchar(20), IN passWord varchar(20))
+BEGIN
+		select (id) from mercury.web_user_table where web_user_table.username = userName and web_user_table.password = passWord;
+END //
+DELIMITER ;
