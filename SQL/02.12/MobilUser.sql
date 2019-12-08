3631165228958428671957e3adfdab09a6623830
Göksel Küçükşahin <gokselkucuksahin@gmail.com>
2019-12-08 03:24:04 +0300

    Update MobilUser.sql

--- SQL/02.12/MobilUser.sql
+++ SQL/02.12/MobilUser.sql
@@ -2,19 +2,19 @@ USE mercury;
 
 create table mobil_user_table(
 id int primary key not null auto_increment,
-username varchar(20) not null,
-password varchar(20) not null
+username varchar(50) not null,
+password varchar(50) not null
 );
 
 DELIMITER //
-CREATE PROCEDURE AddNewMobilUser (IN userName varchar(20), IN password varchar(20))
+CREATE PROCEDURE AddNewMobilUser (IN userName varchar(50), IN password varchar(50))
 BEGIN
 	insert into mercury.mobil_user_table values (null, userName, password);
 END //
 DELIMITER ;
 
 DELIMITER //
-CREATE PROCEDURE CheckMobilUser (IN userName varchar(20))
+CREATE PROCEDURE CheckMobilUser (IN userName varchar(50))
 BEGIN
 	select (id) from mercury.mobil_user_table where mercury.mobil_user_table.username = userName;
 END //
@@ -22,7 +22,7 @@ DELIMITER ;
 
 
 DELIMITER // 
-CREATE PROCEDURE CheckMobilPassword(IN userName varchar(20), IN passWord varchar(20))
+CREATE PROCEDURE CheckMobilPassword(IN userName varchar(50), IN passWord varchar(50))
 BEGIN
 	select (id) from mercury.mobil_user_table where mobil_user_table.username = userName 
     and mobil_user_table.password = passWord;
@@ -30,7 +30,7 @@ END //
 DELIMITER ;
 
 DELIMITER //
-CREATE PROCEDURE DeleteMobilUser (IN userName varchar(20))
+CREATE PROCEDURE DeleteMobilUser (IN userName varchar(50))
 BEGIN
 	set @var := (select id from mercury.mobil_user_table where mercury.mobil_user_table.username = userName);
     delete from mercury.mobil_user_table where mercury.mobil_user_table.id = @var;
